a48997a9218471e7fd83c22eb7ed147979969bd1
Nadja Mielke <mielna01@thu.de>
2021-11-23 12:10:40 +0100

    Activities erstellt

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private static final int REQUESTCODE = 1;
+
+    public void onClick(View view) {   // Die Methode openGalery muss in Jennys Knopf onClick rein
+        Intent i = new Intent(this, grafikeinfuegen.class);
+        startActivityForResult(i, REQUESTCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView grafik = (ImageView) findViewById(R.id.imageView);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                try{
+                    Uri uri = data.getParcelableExtra("pic");
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                   // System.out.println("bitmap");
+                    grafik.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    Bitmap bitmap = null;
+                    System.out.println("konnte Bild nicht laden"); // Stattdessen Errormessage dialog
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/grafikeinfuegen.java
+++ app/src/main/java/com/example/myapplication/grafikeinfuegen.java
@@ -1,2 +1,56 @@
-package com.example.myapplication;public class grafikeinfuegen {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class grafikeinfuegen extends AppCompatActivity {
+    public Uri imgUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openGalery();
+    }
+
+    public void openGalery() {   // Öffnet Galerie und returnt dann ein Bild
+        final int RESULT_GALLERY = 1;
+        Intent galery = new Intent(Intent.ACTION_GET_CONTENT);
+        galery.setType("image/*");
+        // galery.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityForResult(galery, RESULT_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView grafik = (ImageView) findViewById(R.id.imageView);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    this.imgUri = data.getData();
+                    finish();
+                } catch (Exception e) {
+                    this.imgUri = null;
+                    finish();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void finish() {
+        Intent data = new Intent();
+        data.putExtra("pic", this.imgUri);
+                //"pic", this.imgUri.toString());
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
 }
