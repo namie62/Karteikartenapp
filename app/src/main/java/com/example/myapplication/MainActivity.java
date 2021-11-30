ffe667def9afb7e956f8ba6e0fda0a53aa92d2b8
Jennifer Klett <klett@mail.hs-ulm.de>
2021-11-30 11:06:04 +0100

    Erstellung einer Karte

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonabbrechen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonabbrechen = (Button) findViewById(R.id.buttonabbrechen);
+        buttonabbrechen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openPopUpWindow();
+            }
+        });
+    }
+
+    private void openPopUpWindow() {
+        Intent popupwindow = new Intent(MainActivity.this, PopUpWindow.class);
+        startActivity(popupwindow);
     }
 }
