346dea1e1dc8b6c6feae7f3127d920e8b7d87c99
Jennifer Klett <klett@mail.hs-ulm.de>
2021-12-01 16:11:05 +0100

    Verknüpfung der Button im Pop-up-Fenster

--- app/src/main/java/com/example/myapplication/PopUpWindow.java
+++ app/src/main/java/com/example/myapplication/PopUpWindow.java
@@ -2,12 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class PopUpWindow extends AppCompatActivity {
+
+    Button buttonja;
+    Button buttonnein;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,7 @@ public class PopUpWindow extends AppCompatActivity {
         int width = dm.widthPixels;
         int heigth = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.7),(int)(heigth*.3));
+        getWindow().setLayout((int) (width * .7), (int) (heigth * .3));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
@@ -28,5 +38,27 @@ public class PopUpWindow extends AppCompatActivity {
         params.y = -10;
 
         getWindow().setAttributes(params);
+
+        buttonja = (Button) findViewById(R.id.buttonja);
+        buttonja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PopUpWindow.this, MainActivity.class));
+
+            }
+        });
+
+        buttonnein = (Button) findViewById(R.id.buttonnein);
+        buttonnein.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeWindow();
+            }
+        });
+    }
+    public void closeWindow(){
+        this.finish();
     }
 }
+
+
