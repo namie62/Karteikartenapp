0b68f6cbabd9d6cc1dd9ab73058d0aa5507d15eb
Nadja Mielke <mielna01@thu.de>
2022-01-06 13:58:05 +0100

    Layouts für Fach Thema und Karte erstellen eingefügt, angepasst und verbunden

---
+++ app/src/main/java/com/example/myapplication/activities/CreateNewCardToTopicandSubject.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewCardToTopicandSubject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_card_to_topicand_subject);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/activities/CreateTopicPopUp.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateTopicPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_topic_pop_up);
+    }
+}
