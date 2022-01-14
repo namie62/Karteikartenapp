9bcaf5548dc2510b07c12a2120d4b328a434a6ac
cornelia96 <cornelia.hipp1996@gmail.com>
2022-01-14 22:13:52 +0100

    Topic bearbeiten funktioniert

---
+++ app/src/main/java/com/example/myapplication/editactivities/EditTopicActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.editactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTopicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_topic);
+    }
+}
