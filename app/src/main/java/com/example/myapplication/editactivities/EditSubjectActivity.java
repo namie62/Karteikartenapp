3866379ac7dcfba6159ef6b96fa94728cb7b70dd
cornelia96 <cornelia.hipp1996@gmail.com>
2022-01-14 21:18:28 +0100

    EditSubject weiter

--- app/src/main/java/com/example/myapplication/editactivities/EditSubjectActivity.java
+++ app/src/main/java/com/example/myapplication/editactivities/EditSubjectActivity.java
@@ -3,9 +3,8 @@ package com.example.myapplication.editactivities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,8 +12,7 @@ import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.helperclasses.IntentHelper;
-import com.example.myapplication.helperclasses.ListviewHelperClass;
-import com.google.android.material.textfield.TextInputEditText;
+import com.example.myapplication.objectclasses.Subject;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,7 +30,8 @@ public class EditSubjectActivity extends AppCompatActivity {
     private String user;
     private EditText subjectNameEditText, subjectNewPositionEditText;
     private TextView subjectOldPositionTextView;
-    private ArrayList<String> checkedSubjects;
+    private ArrayList<String> allSubjects;
+    private int oldIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,19 +40,48 @@ public class EditSubjectActivity extends AppCompatActivity {
 
         this.user = getIntent().getExtras().getString("user");
         this.selectedSubject = getIntent().getExtras().getString("selectedSubject");
-        this.checkedSubjects = getIntent().getExtras().getStringArrayList("checkedSubjects");
+        this.allSubjects = getIntent().getExtras().getStringArrayList("checkedSubjects");
         this.ih = new IntentHelper(this, user);
 
         FirebaseDatabase flashcardDB = FirebaseDatabase.getInstance("https://karteikar-default-rtdb.europe-west1.firebasedatabase.app/");
         this.reference = flashcardDB.getReference(user);
-
         this.listView = findViewById(R.id.subjects_listView);
+        this.oldIndex = allSubjects.indexOf(selectedSubject);
 
-        subjectNameEditText = findViewById(R.id.subject_edit_text);
+        subjectNameEditText = findViewById(R.id.subject_name_editText);
         subjectNameEditText.setText(selectedSubject);
 
         subjectOldPositionTextView = findViewById(R.id.subject_old_position_textView);
-        subjectOldPositionTextView.setText(String.valueOf(checkedSubjects.indexOf(selectedSubject) + 1));
+        subjectOldPositionTextView.setText(String.valueOf(oldIndex + 1));
+
+        subjectNewPositionEditText = findViewById(R.id.subject_new_position_editTextNumber);
+    }
+
+    public void saveChanges(View view) {
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String newSubjectName = subjectNameEditText.getText().toString();
+                int newIndex =  Integer.parseInt(String.valueOf(subjectNewPositionEditText.getText())) - 1;
+                if (newIndex < allSubjects.size()) {
+                    if (newSubjectName.equals(selectedSubject)) {
+                        allSubjects.remove(selectedSubject);
+                        allSubjects.add(newIndex, selectedSubject);
+                        for (int i=0; i<allSubjects.size(); i++) {
+                            reference.child("subject_sorting").child(String.valueOf(i)).setValue(allSubjects.get(i));
+                        }
+                    } else {
+                        if (newIndex<allSubjects.size()) {
+                            reference.child(newSubjectName).setValue(snapshot.child(selectedSubject).getValue());
+                        }
+                    }
+                }
+            }
+
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/myapplication/objectclasses/Subject.java
+++ app/src/main/java/com/example/myapplication/objectclasses/Subject.java
@@ -1,39 +1,10 @@
 package com.example.myapplication.objectclasses;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Subject {
-    private int sortOrder;
-    private String name;
-
-
-    public Subject(int sortOrder, String name) {
-        this.sortOrder = sortOrder;
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Subject{" +
-                "sortOrder=" + sortOrder +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    public int getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(int sort_order) {
-        this.sortOrder = sort_order;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private HashMap<String, HashMap<Integer, String>> topic;
+    public Subject() {}
 }
