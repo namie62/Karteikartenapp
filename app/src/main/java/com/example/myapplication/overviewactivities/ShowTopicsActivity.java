cf3626669d0285fdee795ff6a34597faa35a674d
cornelia96 <cornelia.hipp1996@gmail.com>
2022-01-21 18:34:56 +0100

    Intenthelper angepasst & Popup angepasst rein gemerget

--- app/src/main/java/com/example/myapplication/overviewactivities/ShowTopicsActivity.java
+++ app/src/main/java/com/example/myapplication/overviewactivities/ShowTopicsActivity.java
@@ -3,7 +3,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,7 +14,6 @@ import com.example.myapplication.R;
 import com.example.myapplication.helperclasses.DeleteStuff;
 import com.example.myapplication.helperclasses.IntentHelper;
 import com.example.myapplication.helperclasses.PDFExport;
-import com.example.myapplication.popups.HintPopUpActivity;
 import com.example.myapplication.helperclasses.ListviewHelperClass;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
